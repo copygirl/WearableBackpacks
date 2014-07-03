e498bbd4f4c0c3ecb9901c7eae6d3b7494797f20
copygirl <halfnerd.copygirl@gmail.com>
2014-07-03 18:50:02 +0200

    Update to copycore's new ModResource

--- src/net/mcft/copy/backpacks/client/BackpackResources.java
+++ src/net/mcft/copy/backpacks/client/BackpackResources.java
@@ -1,6 +1,7 @@
 package net.mcft.copy.backpacks.client;
 
-import net.mcft.copy.core.client.ModResourceLocation;
+import net.mcft.copy.backpacks.WearableBackpacks;
+import net.mcft.copy.core.client.ModResource;
 import net.minecraft.util.ResourceLocation;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -8,10 +9,13 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public final class BackpackResources {
 	
-	public static final ResourceLocation textureBackpack = new ModResourceLocation("textures/models/backpack.png");
-	public static final ResourceLocation textureBackpackOverlay = new ModResourceLocation("textures/models/backpack_overlay.png");
+	public static final ResourceLocation textureBackpack =
+			new ModResource(WearableBackpacks.MOD_ID, "textures/models/backpack.png");
+	public static final ResourceLocation textureBackpackOverlay =
+			new ModResource(WearableBackpacks.MOD_ID, "textures/models/backpack_overlay.png");
 	
-	public static final ResourceLocation modelBackpack = new ModResourceLocation("models/backpack.obj");
+	public static final ResourceLocation modelBackpack =
+			new ModResource(WearableBackpacks.MOD_ID, "models/backpack.obj");
 	
 	private BackpackResources() {  }
 	
