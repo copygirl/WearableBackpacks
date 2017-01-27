419767f525da2bbd057ba51f5b9324d55c0d7a57
copygirl <halfnerd.copygirl@gmail.com>
2017-01-27 20:59:40 +0100

    Move to generic setting validation function
    - Remove abstract validate method
    - Add setValidationFunction(Function<T, String>)
    - Update setValidRange to use validation func
    - Add setValidValues(T...)

--- src/main/java/net/mcft/copy/backpacks/config/SettingInteger.java
+++ src/main/java/net/mcft/copy/backpacks/config/SettingInteger.java
@@ -7,16 +7,15 @@ import net.minecraftforge.common.config.Property;
 
 public class SettingInteger extends Setting<Integer> {
 	
-	public int minValue = Integer.MIN_VALUE;
-	public int maxValue = Integer.MAX_VALUE;
-	
 	public SettingInteger(String category, String name, int defaultValue) {
 		super(category, name, defaultValue);
 	}
 	
+	/** Sets the valid range of values for this integer setting,
+	 *  causing a validation error for values outside this range. */
 	public SettingInteger setValidRange(int min, int max) {
-		minValue = min;
-		maxValue = max;
+		setValidationFunc((value) -> ((value >= min) && (value <= max)) ? null
+			: String.format("Value %s is not in valid range (%s to %s)", value, min, max));
 		return this;
 	}
 	
@@ -32,13 +31,5 @@ public class SettingInteger extends Setting<Integer> {
 	public Integer load(Property property) { return property.getInt(); }
 	@Override
 	public void save(Property property, Integer value) { property.set(value); }
-	
-	@Override
-	public String validate(Integer value) {
-		if ((value < minValue) || (value > maxValue))
-			return String.format("Value %s is not in valid range (%s to %s)",
-									value, minValue, maxValue);
-		return null;
-	}
 	
 }
