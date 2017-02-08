7de8c8004a48f916e194d0c07b871dceb375250b
copygirl <halfnerd.copygirl@gmail.com>
2017-02-08 20:04:40 +0100

    Add version to mod annotation

--- src/main/java/net/mcft/copy/backpacks/WearableBackpacks.java
+++ src/main/java/net/mcft/copy/backpacks/WearableBackpacks.java
@@ -15,11 +15,13 @@ import net.mcft.copy.backpacks.network.BackpacksChannel;
 // TODO: Add achievement(s)! <3
 
 @Mod(modid = WearableBackpacks.MOD_ID, name = WearableBackpacks.MOD_NAME,
+     version = WearableBackpacks.VERSION,
      guiFactory = "net.mcft.copy.backpacks.client.BackpacksGuiFactory")
 public class WearableBackpacks {
 	
 	public static final String MOD_ID   = "wearablebackpacks";
 	public static final String MOD_NAME = "Wearable Backpacks";
+	public static final String VERSION  = "@VERSION@";
 	
 	@Instance
 	public static WearableBackpacks INSTANCE;
