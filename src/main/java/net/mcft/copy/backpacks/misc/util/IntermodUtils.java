2dec420e592d4f3bef9dacff43e31c0dfbb3d43f
copygirl <copygirl@mcft.net>
2018-01-22 00:10:17 +0100

    Fix Quark colored runes support
    I blame @lumien231 (Vazkii/Quark#738) :D

--- src/main/java/net/mcft/copy/backpacks/misc/util/IntermodUtils.java
+++ src/main/java/net/mcft/copy/backpacks/misc/util/IntermodUtils.java
@@ -1,19 +1,21 @@
 package net.mcft.copy.backpacks.misc.util;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
-import net.mcft.copy.backpacks.WearableBackpacks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Loader;
+
+import net.mcft.copy.backpacks.WearableBackpacks;
 
 public final class IntermodUtils {
 	
 	private IntermodUtils() {  }
 	
-	private static boolean getRuneColorCached = false;
-	private static Method setTargetStackMethod;
-	private static Method getColorMethod;
+	private static final int DEFAULT_ENCHANTMENT_COLOR = 0xFF8040CC;
+	private static boolean getRuneColorCached  = false;
+	private static Method setTargetStackMethod = null;
+	private static Method getColorMethod       = null;
 	/** Returns the Quark rune color for this item or the default enchant glint
 	 *  color if Quark isn't present or the item doesn't have a custom rune color. */
 	public static int getRuneColor(ItemStack stack) {
@@ -22,28 +24,28 @@ public final class IntermodUtils {
 				try {
 					Class<?> clazz = Class.forName("vazkii.quark.misc.feature.ColorRunes");
 					setTargetStackMethod = clazz.getMethod("setTargetStack", ItemStack.class);
-					getColorMethod = clazz.getMethod("getColor");
+					getColorMethod = Arrays.stream(clazz.getMethods())
+						.filter(m -> "getColor".equals(m.getName()))
+						.findAny().orElse(null);
 				} catch (ClassNotFoundException |
 				         NoSuchMethodException ex) {
 					WearableBackpacks.LOG.error("Error while fetching Quark ColorRunes methods", ex);
 				}
-			} else {
-				setTargetStackMethod = null;
-				getColorMethod = null;
 			}
 			getRuneColorCached = true;
 		}
-		if (setTargetStackMethod == null)
-			return 0xFF8040CC;
+		if ((setTargetStackMethod == null) || (getColorMethod == null))
+			return DEFAULT_ENCHANTMENT_COLOR;
 		try {
 			setTargetStackMethod.invoke(null, stack);
-			return (int)getColorMethod.invoke(null);
-		} catch (IllegalAccessException |
-		         InvocationTargetException ex) {
+			return (getColorMethod.getParameterCount() == 0)
+				? (int)getColorMethod.invoke(null)
+				: (int)getColorMethod.invoke(null, DEFAULT_ENCHANTMENT_COLOR);
+		} catch (Exception ex) {
 			WearableBackpacks.LOG.error("Error while invoking Quark ColorRunes methods", ex);
 			setTargetStackMethod = null;
-			getColorMethod = null;
-			return 0xFF8040CC;
+			getColorMethod       = null;
+			return DEFAULT_ENCHANTMENT_COLOR;
 		}
 	}
 	
