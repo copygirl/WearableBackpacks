002a24df5940ad0ea6a5ceba6f6841425a3efc98
InsomniaKitten <admin@designdevils.co.uk>
2019-04-23 18:15:54 +0100

    Clean slate

--- src/main/java/net/mcft/copy/backpacks/api/IBackpackData.java
+++
@@ -1,9 +0,0 @@
-package net.mcft.copy.backpacks.api;
-
-import net.minecraft.nbt.NBTBase;
-
-import net.minecraftforge.common.util.INBTSerializable;
-
-/** Interface for classes which contain backpack data that may be stored on
- *  an equipped or placed backpack, for example one containing its items. */
-public interface IBackpackData extends INBTSerializable<NBTBase> {  }
