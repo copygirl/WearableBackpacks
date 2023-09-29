2a72c5ea4ef348bdaa811d33d9f7106827bee876
Mysticpasta1 <ethana5tc@outlook.com>
2023-09-29 11:35:40 -0500

    fix crash with sodium but mixin is still broken it seems

--- src/main/java/dev/sapphic/wearablebackpacks/mixin/client/ItemRendererMixin.java
+++ src/main/java/dev/sapphic/wearablebackpacks/mixin/client/ItemRendererMixin.java
@@ -9,25 +9,33 @@ import net.minecraft.item.ItemStack;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
-@Mixin(ItemRenderer.class)
+@Mixin(value = ItemRenderer.class, priority = 1100) //priority needed to keep it from crashing with sodium
 abstract class ItemRendererMixin {
+  private ItemStack stack;
+
   @Redirect(
-    method = "renderBakedItemQuads(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Ljava/util/List;Lnet/minecraft/item/ItemStack;II)V",
+    method = "renderBakedItemQuads",
     at = @At(value = "INVOKE", opcode = Opcodes.INVOKEVIRTUAL,
       target = "Lnet/minecraft/client/render/VertexConsumer;quad(Lnet/minecraft/client/util/math/MatrixStack$Entry;Lnet/minecraft/client/render/model/BakedQuad;FFFII)V"),
     require = 1, allow = 1)
   private void swapOverlayPipeline(
-    final VertexConsumer pipeline, final MatrixStack.Entry entry, final BakedQuad quad, final float red, final float green, final float blue, final int light, final int overlay,
-    final MatrixStack matrices, final VertexConsumer vertices, final List<BakedQuad> quads, final ItemStack stack, final int light1, final int overlay1
-  ) {
+          VertexConsumer pipeline, MatrixStack.Entry entry, BakedQuad quad, float red, float green, float blue, int light, int overlay) {
     if ((pipeline instanceof DualVertexConsumerAccessor) && (stack.getItem() == Backpacks.backpackItem) && !quad.hasColor()) {
 //            ((DualVertexConsumerAccessor) pipeline).getSecond().quad(entry, quad, red, green, blue, light, overlay);
     } else {
       pipeline.quad(entry, quad, red, green, blue, light, overlay);
     }
+  }
+
+  //Done so the mixin doesn't crash from extra parameters
+  @Inject(method = "renderBakedItemQuads", at = @At(value = "HEAD"))
+  private void getStack(MatrixStack matrices, VertexConsumer vertices, List<BakedQuad> quads, ItemStack stack, int light, int overlay, CallbackInfo ci) {
+      this.stack = stack;
   }
 }
